58da5816104c3475aff740d0ff1d2a9cbbba9a14
Moki <mokiong1427@gmail.com>
2021-11-06 16:07:47 +0800

    Add 1200px max width for layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/nav/Navbar';
-import { Fragment } from 'react';
 import Layout from '../components/layout/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 
 import Specialty from '../components/body/Specialty';
 import MenuHeader from '../components/header/Header';
-import Layout from '../components/layout/Layout';
 
 const Home: NextPage = () => {
     return (
