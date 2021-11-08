f3da7675dbd6fa8753e46d4a5297ecc060011e99
Moki <mokiong1427@gmail.com>
2021-11-08 18:20:55 +0800

    Fix component structure for heading and tabs

--- components/header/MenuHeader.tsx
+++ components/header/MenuHeader.tsx
@@ -1,114 +1,16 @@
-import { useState } from 'react';
-
-import Tabs from '../body/Tabs';
-import DownArrow from '../Icon/DownArrow';
 import Container from '../utils/Container';
 
 import classes from './header.module.css';
 
-const Cards = [
-    'Brand Identity',
-    'Brand Identity',
-    'Web Design',
-    'Digital Designs',
-    'Motion graphics',
-    'Web Development',
-    'Social Media Content/Campaign',
-    'Photo/Video',
-    'Print and Packaging',
-    'Brand research & story',
-    'Fashion design',
-    'mock-ups',
-];
-const TabItems = [
-    'Brand Messaging',
-    'Brand Story',
-    'Brand Research',
-    'Brand Strategy',
-    'Social Media',
-    'Copywriting',
-];
-
 const MenuHeader = () => {
-    const [chosenCourse, setChosenCourse] = useState<string>('Appetizers');
-
     return (
-        <>
-            <Container bgColor="black">
-                <div className={classes.container}>
-                    <div className={classes.headerContainer}>
-                        <h1 className={classes.headerText}>
-                            Tell us what you&apos;re craving for.
-                        </h1>
-                    </div>
-                    <div className={classes.tabs}>
-                        <div className={classes.tabContainer}>
-                            <div className={classes.icon}>
-                                {chosenCourse === 'Appetizers' && (
-                                    <DownArrow color="#f89811" size="3rem" />
-                                )}
-                            </div>
-                            {chosenCourse === 'Appetizers' ? (
-                                <>
-                                    <button
-                                        onClick={() => {
-                                            setChosenCourse('Appetizers');
-                                        }}
-                                        className={`${classes.btn} ${classes.activeBtn}`}
-                                    >
-                                        APPETIZERS
-                                    </button>
-                                </>
-                            ) : (
-                                <button
-                                    onClick={() => {
-                                        setChosenCourse('Appetizers');
-                                    }}
-                                    className={classes.btn}
-                                >
-                                    APPETIZERS
-                                </button>
-                            )}
-                        </div>
-                        <div className={classes.tabContainer}>
-                            <div className={classes.icon}>
-                                {chosenCourse === 'Main' && (
-                                    <DownArrow color="#f89811" size="3rem" />
-                                )}
-                            </div>
-                            <button
-                                onClick={() => {
-                                    setChosenCourse('Main');
-                                }}
-                                className={classes.btn}
-                            >
-                                MAIN COURSE
-                            </button>
-                        </div>
-                        <div className={classes.tabContainer}>
-                            <div className={classes.icon}>
-                                {chosenCourse === 'Dessert' && (
-                                    <DownArrow color="#f89811" size="3rem" />
-                                )}
-                            </div>
-                            <button
-                                onClick={() => {
-                                    setChosenCourse('Dessert');
-                                }}
-                                className={classes.btn}
-                            >
-                                DESSERT
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </Container>
-            <Container bgColor="#f7ede1">
-                <section>
-                    <Tabs title={chosenCourse} cards={Cards} menu={TabItems} />
-                </section>
-            </Container>
-        </>
+        <Container bgColor="black">
+            <div className={classes.container}>
+                <h1 className={classes.headerText}>
+                    Tell us what you&apos;re craving for.
+                </h1>
+            </div>
+        </Container>
     );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 
 import Specialty from '../components/body/Specialty';
+import TabMenu from '../components/body/TabMenu';
 import MenuHeader from '../components/header/MenuHeader';
 
 const Home: NextPage = () => {
     return (
         <>
             <MenuHeader />
+            <TabMenu />
             <Specialty />
         </>
     );
