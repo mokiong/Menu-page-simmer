66384342969028d2a896bdba3a42f663f7a7fbf4
Moki <mokiong1427@gmail.com>
2021-11-09 14:18:20 +0800

    Add isFullWidth prop for container component

--- components/utils/Container.tsx
+++ components/utils/Container.tsx
@@ -4,12 +4,19 @@ import classes from './container.module.css';
 
 interface ContainerInterface {
     bgColor: string;
+    isFullWidth?: boolean;
 }
 
-const Container: FC<ContainerInterface> = ({ bgColor, children }) => {
+const Container: FC<ContainerInterface> = ({
+    isFullWidth = false,
+    bgColor,
+    children,
+}) => {
     return (
         <div style={{ backgroundColor: bgColor }}>
-            <div className={classes.container}>{children}</div>
+            <div className={!isFullWidth ? classes.container : ''}>
+                {children}
+            </div>
         </div>
     );
 };
